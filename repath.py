3d172596384665c2c13d3a4b8e506e2fbe15d2e1
Simarilius <simarilius@yahoo.com>
2023-01-27 18:24:35 +0000

    checked it with a big list of mis

--- repath.py
+++ repath.py
@@ -9,34 +9,72 @@ import glob
 
 def replace_mis(node):
     if type(node) is dict:
-        print("is dict")
+        #print("is dict")
         for k in node.keys():
-            print(k)
+            #print(k)
             if k=='DepotPath' and node['DepotPath'] in old_mis:
                 print("Replacing ",node[k]," with ", new_mis[old_mis.index(node['DepotPath'])])
                 node['DepotPath']=new_mis[old_mis.index(node['DepotPath'])]
             if type( node[k]) is dict:               
-                print(k, "is dict")
+                #print(k, "is dict")
                 replace_mis(node[k])
             if type( node[k]) is list:               
-                print(k, "is list")
+                #print(k, "is list")
                 replace_mis(node[k])
     if type(node) is list:
-        print("is list")
+        #print("is list")
         for li in node:
-            print(li)
+            #print(li)
             if type(li) is dict:
-                print(li, "is dict")
+                #print(li, "is dict")
                 replace_mis(li)
 
 
 
 project_path='F:\\cpmod\\ids_irezumi_tattoos\\source\\raw\\'
-outpath=
 
-old_mis=['base\\characters\\common\\skin\\character_mat_instance\\female\\body\\female_01_ca_pale.mi']
 
-new_mis=['island_dancer\\custom\\skin\\body\\woman_average\\parameters\\custom\\female_01_ca_pale.mi']
+old_mis=['base\\characters\\common\\skin\\character_mat_instance\\female\\body\\female_01_ca_pale_00_warm_ivory.mi',
+'base\\characters\\common\\skin\\character_mat_instance\\female\\body\\female_02_ca_limestone.mi',
+'base\\characters\\common\\skin\\character_mat_instance\\female\\body\\female_02_ca_limestone_00_beige.mi',
+'base\\characters\\common\\skin\\character_mat_instance\\female\\body\\female_03_ca_senna.mi',
+'base\\characters\\common\\skin\\character_mat_instance\\female\\body\\female_03_ca_senna_00_amber.mi',
+'base\\characters\\common\\skin\\character_mat_instance\\female\\body\\female_03_ca_senna_01_honey.mi',
+'base\\characters\\common\\skin\\character_mat_instance\\female\\body\\female_03_ca_senna_02_band.mi',
+'base\\characters\\common\\skin\\character_mat_instance\\female\\body\\female_04_ca_almond.mi',
+'base\\characters\\common\\skin\\character_mat_instance\\female\\body\\female_04_ca_almond_00_umber.mi',
+'base\\characters\\common\\skin\\character_mat_instance\\female\\body\\female_05_bl_espresso.mi',
+'base\\characters\\common\\skin\\character_mat_instance\\female\\body\\female_06_bl_dark.mi',
+'base\\characters\\common\\skin\\character_mat_instance\\female\\body\\female_npc_01_pale_dirt.mi',
+'base\\characters\\common\\skin\\character_mat_instance\\female\\body\\female_npc_02_black_dirt.mi',
+'base\\characters\\common\\skin\\character_mat_instance\\female\\body\\female_npc_03_limestone_sick.mi',
+'base\\characters\\common\\skin\\character_mat_instance\\female\\body\\female_npc_04_almond_sick.mi',
+'base\\characters\\common\\skin\\character_mat_instance\\female\\body\\female_npc_05_senna_valentino.mi',
+'base\\characters\\common\\skin\\character_mat_instance\\female\\body\\female_npc_05_senna_valentino_01.mi',
+'base\\characters\\common\\skin\\character_mat_instance\\female\\body\\female_npc_06_limestone_tyger.mi',
+'base\\characters\\common\\skin\\character_mat_instance\\female\\body\\female_npc_dead_01.mi',
+'base\\characters\\common\\skin\\character_mat_instance\\female\\body\\female_npc_dead_02.mi']
+
+new_mis=['island_dancer\\custom\\skin\\body\\woman_average\\parameters\\custom\\female_01_ca_pale_00_warm_ivory.mi',
+'island_dancer\\custom\\skin\\body\\woman_average\\parameters\\custom\\female_02_ca_limestone.mi',
+'island_dancer\\custom\\skin\\body\\woman_average\\parameters\\custom\\female_02_ca_limestone_00_beige.mi',
+'island_dancer\\custom\\skin\\body\\woman_average\\parameters\\custom\\female_03_ca_senna.mi',
+'island_dancer\\custom\\skin\\body\\woman_average\\parameters\\custom\\female_03_ca_senna_00_amber.mi',
+'island_dancer\\custom\\skin\\body\\woman_average\\parameters\\custom\\female_03_ca_senna_01_honey.mi',
+'island_dancer\\custom\\skin\\body\\woman_average\\parameters\\custom\\female_03_ca_senna_02_band.mi',
+'island_dancer\\custom\\skin\\body\\woman_average\\parameters\\custom\\female_04_ca_almond.mi',
+'island_dancer\\custom\\skin\\body\\woman_average\\parameters\\custom\\female_04_ca_almond_00_umber.mi',
+'island_dancer\\custom\\skin\\body\\woman_average\\parameters\\custom\\female_05_bl_espresso.mi',
+'island_dancer\\custom\\skin\\body\\woman_average\\parameters\\custom\\female_06_bl_dark.mi',
+'island_dancer\\custom\\skin\\body\\woman_average\\parameters\\custom\\female_npc_01_pale_dirt.mi',
+'island_dancer\\custom\\skin\\body\\woman_average\\parameters\\custom\\female_npc_02_black_dirt.mi',
+'island_dancer\\custom\\skin\\body\\woman_average\\parameters\\custom\\female_npc_03_limestone_sick.mi',
+'island_dancer\\custom\\skin\\body\\woman_average\\parameters\\custom\\female_npc_04_almond_sick.mi',
+'island_dancer\\custom\\skin\\body\\woman_average\\parameters\\custom\\female_npc_05_senna_valentino.mi',
+'island_dancer\\custom\\skin\\body\\woman_average\\parameters\\custom\\female_npc_05_senna_valentino_01.mi',
+'island_dancer\\custom\\skin\\body\\woman_average\\parameters\\custom\\female_npc_06_limestone_tyger.mi',
+'island_dancer\\custom\\skin\\body\\woman_average\\parameters\\custom\\female_npc_dead_01.mi',
+'island_dancer\\custom\\skin\\body\\woman_average\\parameters\\custom\\female_npc_dead_02.mi']
 
 
 jsons= glob.glob(project_path+"\**\*.mesh.json", recursive = True)
