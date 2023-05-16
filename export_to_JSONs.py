b976335ecde0de6fcbcecf74874aecd3ad7a0a5f
Simarilius-uk <simarilius@yahoo.com>
2023-05-16 13:30:56 +0100

    QA fixes
    mana found problems, should be fixed now.

--- export_to_JSONs.py
+++ export_to_JSONs.py
@@ -27,13 +27,19 @@ from mathutils import Vector, Matrix, Quaternion
 C = bpy.context
 
 #Set this to your project directory
-project = 'F:\\CPmod\\meshdecal_parralax'
+project = 'F:\\CPmod\\mana'
 path = os.path.join(project,'source\\raw\\base')
 
 #its currently set to output the modified jsons to an output folder in the project dir (create one before running)
 #you can change this to a path if you prefer
 outpath = os.path.join(project,'output')
 
+def checkexists(meshname, Masters):
+    groupname = os.path.splitext(os.path.split(meshname)[-1])[0]
+    if groupname in Masters.children.keys() and len(Masters.children[groupname].objects)>0:
+        return True
+    else:
+        return False
 
 def get_pos(inst):
     pos=[0,0,0]
@@ -117,6 +123,20 @@ def set_pos(inst,obj):
         inst['translation']['Y'] = float("{:.9g}".format(obj.location[1]*100))
         inst['translation']['Z'] = float("{:.9g}".format(obj.location[2]*100))
 
+def set_z_pos(inst,obj):  
+    #print(inst)  
+    if 'Position'in inst.keys():
+        if 'Properties' in inst['Position'].keys():
+            inst['Position']['Properties']['Z'] = float("{:.9g}".format(obj.location[2]*100))
+        else:
+            if 'X' in inst['Position'].keys():
+                inst['Position']['Z'] = float("{:.9g}".format(obj.location[2]*100))
+            else:
+                inst['Position']['z'] = float("{:.9g}".format(obj.location[2]*100))
+    elif 'position' in inst.keys():
+        inst['position']['Z'] = float("{:.9g}".format(obj.location[2]*100))
+    elif 'translation' in inst.keys():
+        inst['translation']['Z'] = float("{:.9g}".format(obj.location[2]*100))
 
 def set_rot(inst,obj):
     if 'Orientation' in inst.keys():
@@ -180,6 +200,17 @@ def find_col(NodeIndex,Inst_idx,Sector_coll):
         inst=[x for x in col if x['instance_idx']==Inst_idx]
         return inst[0]
 
+def find_wIDMN_col(NodeIndex,tl_inst_idx, sub_inst_idx,Sector_coll):
+    #print('Looking for NodeIndex ',NodeIndex,' Inst_idx ',Inst_idx, ' in ',Sector_coll)
+    col=[x for x in Sector_coll.children if x['nodeIndex']==NodeIndex]
+    if len(col)==0:
+        return None
+    elif len(col)==1:
+        return col[0]
+    else: 
+        inst=[x for x in col if x['tl_instance_idx']==tl_inst_idx and x['sub_instance_idx']==sub_inst_idx]
+        return inst[0]
+
 def find_decal(NodeIndex,Inst_idx,Sector_coll):
     #print('Looking for NodeIndex ',NodeIndex,' Inst_idx ',Inst_idx, ' in ',Sector_coll)
     col=[x for x in Sector_coll.objects if x['nodeIndex']==NodeIndex]
@@ -211,9 +242,10 @@ def createNodeData(t, col, nodeIndex, obj, ID):
 jsons = glob.glob(path+"\**\*.streamingsector.json", recursive = True)
 
 Masters=bpy.data.collections.get("MasterInstances")
-neg_cube=''
+
+neg_cube=None
 if 'neg_cube' not in Masters.objects.keys():
-    bpy.ops.mesh.primitive_cube_add(size=.01, scale=(-1,-1,-1),location=(0,0,-1000))
+    bpy.ops.mesh.primitive_cube_add(size=.01, scale=(-1,-1,-1),location=(0,0,-10))
     neg_cube=C.selected_objects[0]
     neg_cube.name='neg_cube'
     neg_cube.users_collection[0].objects.unlink(neg_cube)
@@ -232,17 +264,30 @@ for filepath in jsons:
     nodes = j["Data"]["RootChunk"]["nodes"]
     t=j['Data']['RootChunk']['nodeData']['Data']
     sectorName=os.path.basename(filepath)[:-5]
-
+    if sectorName not in bpy.data.collections.keys():
+        break
+    print('Updating sector ',sectorName)
     Sector_coll=bpy.data.collections.get(sectorName)
     if 'filepath' not in Sector_coll.keys():
         Sector_coll['filepath']=filepath
+    #print(filepath)
+    #print(len(nodes))
     Sector_additions_coll=bpy.data.collections.get(sectorName+'_new')
+    wIMNs=0
     for i,e in enumerate(nodes):
         data = e['Data']
         type = data['$type']
         match type:
             case 'worldInstancedMeshNode' :
+                wIMNs+=1
+                #print(wIMNs)
                 meshname = data['mesh']['DepotPath'] 
+                #if 'chopstick' in meshname:
+                #    print('worldInstancedMeshNode - ',meshname)
+                if not checkexists(meshname, Masters):
+                    print(meshname, ' not found in masters')
+                    continue
+                
                 num=data['worldTransformsBuffer']['numElements']
                 start=data['worldTransformsBuffer']['startIndex']
                 if(meshname != 0):
@@ -262,6 +307,8 @@ for filepath in jsons:
                         if Sector_additions_coll:
                             Sector_additions_coll['Inst_bufferID']=bufferID
                         obj_col=find_col(i,idx,Sector_coll)    
+                        if 'chopstick' in meshname:
+                            print(obj_col,len(obj_col.objects))
                         if obj_col and inst_trans:
                             if len(obj_col.objects)>0:
                                 obj=obj_col.objects[0]
@@ -270,7 +317,7 @@ for filepath in jsons:
                                 set_scale(inst_trans,obj)
                             else:
                                 obj=neg_cube
-                                set_pos(inst_trans,obj)
+                                set_z_pos(inst_trans,obj)
             case 'worldStaticDecalNode':
                 #print('worldStaticDecalNode')
                 instances = [x for x in t if x['NodeIndex'] == i]
@@ -282,8 +329,8 @@ for filepath in jsons:
                         set_scale(inst,obj)
                     else:
                         obj=neg_cube
-                        set_pos(inst,obj)
-            case 'worldStaticMeshNode' | 'worldBuildingProxyMeshNode' | 'worldGenericProxyMeshNode'| 'worldTerrainProxyMeshNode': 
+                        set_z_pos(inst,obj)
+            case   'worldStaticMeshNode' | 'worldBuildingProxyMeshNode' | 'worldGenericProxyMeshNode'| 'worldTerrainProxyMeshNode': 
                 if isinstance(e, dict) and 'mesh' in data.keys():
                     meshname = data['mesh']['DepotPath']
                     #print('Mesh name is - ',meshname, e['HandleId'])
@@ -300,7 +347,28 @@ for filepath in jsons:
                                     set_scale(inst,obj)
                                 else:
                                     obj=neg_cube
+                                    set_z_pos(inst,obj)
+            case  'worldEntityNode':
+                if isinstance(e, dict) and 'entityTemplate' in data.keys():
+                    entname = data['entityTemplate']['DepotPath']
+                    
+                    if(entname != 0):
+                        instances = [x for x in t if x['NodeIndex'] == i]
+                        for idx,inst in enumerate(instances):
+                            obj_col=find_col(i,idx,Sector_coll)
+                            #print(obj_col)
+                            if obj_col:
+                                if len(obj_col.objects)>0:
+                                    obj=obj_col.objects[0]
                                     set_pos(inst,obj)
+                                    set_rot(inst,obj)
+                                    set_scale(inst,obj)
+                                else:
+                                    obj=neg_cube
+                                    set_z_pos(inst,obj)                                    
+                                    
+                                    
+                                    
             case 'worldInstancedDestructibleMeshNode':
                 #print('worldInstancedDestructibleMeshNode',i)
                 if isinstance(e, dict) and 'mesh' in data.keys():
@@ -308,12 +376,13 @@ for filepath in jsons:
                     num=data['cookedInstanceTransforms']['numElements']
                     start=data['cookedInstanceTransforms']['startIndex']
                     instances = [x for x in t if x['NodeIndex'] == i]
-                    for inst in instances:
+                    for tlidx,inst in enumerate(instances):
                         for idx in range(start, start+num):
                             bufferID=0
+                            basic_trans=None
                             # Transforms are inside the cookedInstanceTransforms in a buffer
                             if 'Data' in data['cookedInstanceTransforms']['sharedDataBuffer'].keys():
-                                inst_trans=data['cookedInstanceTransforms']['sharedDataBuffer']['Data']['buffer']['Data']['Transforms'][idx]
+                                basic_trans=data['cookedInstanceTransforms']['sharedDataBuffer']['Data']['buffer']['Data']['Transforms'][idx]
 
                             # Transforms are in a shared buffer in another node, so get the reference and find the transform data                    
                             elif 'HandleRefId' in data['cookedInstanceTransforms']['sharedDataBuffer'].keys():
@@ -322,43 +391,54 @@ for filepath in jsons:
                                 for n in nodes:
                                     if n['HandleId']==str(bufferID-1):
                                         ref=n
-                                inst_trans = ref['Data']['cookedInstanceTransforms']['sharedDataBuffer']['Data']['buffer']['Data']['Transforms'][idx]   
-                                                  
+                                basic_trans = ref['Data']['cookedInstanceTransforms']['sharedDataBuffer']['Data']['buffer']['Data']['Transforms'][idx]   
+                                #print(basic_trans)                  
                             else :
                                 print(e)
                             # store the bufferID for when we add new stuff.
                             if Sector_additions_coll:
                                 Sector_additions_coll['Dest_bufferID']=bufferID
-                                print('Setting Dest_bufferID to ',bufferID)
-
+                                #print('Setting Dest_bufferID to ',bufferID)
+                            
                             # the Transforms are stored as 2 parts, a basic transform applied to all the instances and individual ones per instance
                             # lets get the basic one so we can calculate the instance one.
+                            basic_pos =Vector(get_pos(basic_trans))
+                            basic_rot =Quaternion(get_rot(basic_trans))
+                            basic_scale =Vector((1,1,1))
+                            basic_matr=Matrix.LocRotScale(basic_pos,basic_rot,basic_scale)
+                            basic_matr_inv=basic_matr.inverted()
+                            
+                            # Never modify the basic on as other nodes may be referencing it. (its normally 0,0,0 anyway)
                             inst_pos =Vector(get_pos(inst))
                             inst_rot =Quaternion(get_rot(inst))
                             inst_scale =Vector((1,1,1))
                             inst_m=Matrix.LocRotScale(inst_pos,inst_rot,inst_scale)
-                            inst_m_inv=inst_m.inverted()
+                            
 
-                            obj_col=find_col(i,idx,Sector_coll)
+                            obj_col=find_wIDMN_col(i,tlidx,idx,Sector_coll)
                             if obj_col:
                                 if len(obj_col.objects)>0:
                                     obj=obj_col.objects[0]
                                     
-                                    inst_trans_m = inst_m_inv @ obj.matrix_local 
+                                    inst_trans_m = obj.matrix_world @ basic_matr_inv
+                                    obj_trans = obj.location
                                     pos=inst_trans_m.translation
-                                    inst_trans['position']['X']=pos[0]*100
-                                    inst_trans['position']['Y']=pos[1]*100
-                                    inst_trans['position']['Z']=pos[2]*100
+                                    #pos=obj_trans-basic_pos
+                                    inst['Position']['X'] = float("{:.9g}".format(pos[0]*100))
+                                    inst['Position']['Y'] = float("{:.9g}".format(pos[1]*100))
+                                    inst['Position']['Z'] = float("{:.9g}".format(pos[2]*100))
                                     quat=inst_trans_m.to_quaternion()
-                                    inst_trans['orientation']['r']=-quat[0]
-                                    inst_trans['orientation']['i']=-quat[1]
-                                    inst_trans['orientation']['j']=-quat[2]
-                                    inst_trans['orientation']['k']=-quat[3]
+                                    inst['Orientation']['r'] = float("{:.9g}".format(quat[0] ))
+                                    inst['Orientation']['i'] = float("{:.9g}".format(quat[1] ))
+                                    inst['Orientation']['j'] = float("{:.9g}".format(quat[2] ))
+                                    inst['Orientation']['k'] = float("{:.9g}".format(quat[3] ))
+                                    scale=inst_trans_m.to_scale()
+                                    inst['Scale']['X']=float("{:.9g}".format(scale.x*100))
+                                    inst['Scale']['Y']=float("{:.9g}".format(scale.y*100))
+                                    inst['Scale']['Z']=float("{:.9g}".format(scale.z*100))
                                 else:
-                                    inst_trans['position']['X']=0
-                                    inst_trans['position']['Y']=0
-                                    inst_trans['position']['Z']=-3000
-
+                                    inst['Position']['Z']=-300
+    print(wIMNs)
                                     
 #       __   __          __      __  ___       ___  ___ 
 #  /\  |  \ |  \ | |\ | / _`    /__`  |  |  | |__  |__  
@@ -374,11 +454,17 @@ for filepath in jsons:
         for col in Sector_additions_coll.children:
             if 'nodeIndex' in col.keys() and col['sectorName']==sectorName and len(col.objects)>0:
                 match col['nodeType']:
-                    case 'worldStaticMeshNode' | 'worldBuildingProxyMeshNode' | 'worldGenericProxyMeshNode' | 'worldTerrainProxyMeshNode':
+                    case 'worldStaticMeshNode' | 'worldStaticDecalNode' | 'worldBuildingProxyMeshNode' | 'worldGenericProxyMeshNode' | 'worldTerrainProxyMeshNode':
                         obj=col.objects[0]
                         createNodeData(t, col, col['nodeIndex'], obj,ID)
                         ID+=1
-                        
+                    case 'worldEntityNode':
+                        new_ni=len(nodes)
+                        nodes.append(copy.deepcopy(nodes[col['nodeIndex']]))
+                        obj=col.objects[0]
+                        createNodeData(t, col, new_ni, obj,ID)
+                        ID+=1
+                                      
                     case 'worldInstancedMeshNode':
                         obj=col.objects[0]
                         nodeIndex=col['nodeIndex']
